01bd259b8d5d4774e1ddc5875195e8a4589a538f
SrinivasaOdela <116736887+SrinivasaOdela@users.noreply.github.com>
2022-11-22 21:05:34 -0500

    Update streamlit_app.py
    added a pick list using multiselect method

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,7 @@ st.text('🐔 Hard Boiled free-range eggs')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # my_fruit_list is a pandas data_frame
+
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#pick list 
 st.dataframe(my_fruit_list)
