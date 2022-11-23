6e73c4b88863970cb579ad232152de580d187588
SrinivasaOdela <116736887+SrinivasaOdela@users.noreply.github.com>
2022-11-22 20:57:29 -0500

    Update streamlit_app.py
    used text instead of write method

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title('My Healthy Diner')
 
 st.subheader('Breakfast Menu')
-st.write('Omega 3 & Blueberry Oatmeal')
-st.write('Kale, Spinach & Rocket smoothie')
-st.write('Hard Boiled free-range eggs')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket smoothie')
+st.text('Hard Boiled free-range eggs')
