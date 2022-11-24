0aaa96cbd085de5a0db108ad8faa3dae04bcbf01
SrinivasaOdela <116736887+SrinivasaOdela@users.noreply.github.com>
2022-11-23 22:47:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,60 +4,60 @@
 #import snowflake.connector 
 from urllib.error import URLError
 
-st.title('My Healthy Diner')
+streamlit.title('My Healthy Diner')
 
-st.subheader('Breakfast Favorites')
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket smoothie')
-st.text('🐔 Hard Boiled free-range eggs')
+streamlit.subheader('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔 Hard Boiled free-range eggs')
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # my_fruit_list is a pandas data_frame
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Displaying only selected fruits in the grid
-fruits_selected= st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 
-#st.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
 
 #########################################
 # Add on package
-st.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
-#st.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 #########################################
 
-st.stop()
+streamlit.stop()
 
 #--------------------------------------------------------------#
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
 
-st.header("The fruit load list contains:")
-st.dataframe(my_data_rows)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 #--------------------------------------------------------------#
 
-fruit_add = st.text_input('What fruit would you like ADD?','jackfruit')
-st.write('Thanks for adding ', fruit_add)
+fruit_add = streamlit.text_input('What fruit would you like ADD?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_add)
 
 my_cur.execute("insert into fruit_load_list values ('from st')")
